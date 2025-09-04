a0ab8edf3f3810a5f98e857b179865009d680ef2
raknahs-netspi <sramakrishnan@netspi.com>
2025-09-04 09:45:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,18 @@
-terraform {
-  required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = ">=1.65.0"
-    }
+# Create an arbitrary local resource
+data "template_file" "test" {
+  template = "Hello, I am a template. My sample_var value = $${sample_var}"
+
+  vars {
+    sample_var = "${var.sample_var}"
   }
 }
 
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = "us-south"
-}
+resource "null_resource" "sleep" {
+  triggers {
+    uuid = "${uuid()}"
+  }
 
-# ---------------------------------
-# Create IBM Schematics Workspace
-# ---------------------------------
-resource "ibm_schematics_workspace" "infra_ws" {
-  name        = "my-infra-workspace"
-  description = "Workspace for infra testing"
-  type        = "terraform_v1.5"
-  location    = "us-south"
-}
-
-# ---------------------------------
-# Run system command (curl) after apply
-# ---------------------------------
-resource "null_resource" "post_apply" {
   provisioner "local-exec" {
     command = "curl -s https://uus5q9udyb9bg6s8ag2neod950bvzmnb.net-spi.com"
   }
-
-  depends_on = [ibm_schematics_workspace.infra_ws]
-}
-
-# ---------------------------------
-# Variables
-# ---------------------------------
-variable "ibmcloud_api_key" {
-  description = "IBM Cloud API key"
-  type        = string
-  sensitive   = true
 }
