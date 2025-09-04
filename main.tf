29561bfb8b6f13e28ebb04f649cb96655295d490
raknahs-netspi <sramakrishnan@netspi.com>
2025-09-04 12:08:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,94 @@
-# Create an arbitrary local resource
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars {
-    sample_var = "${var.sample_var}"
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = ">=1.65.0"
+    }
   }
 }
 
-resource "null_resource" "sleep" {
-  triggers {
-    uuid = "${uuid()}"
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = "us-south"
+}
+
+# -----------------------------
+# Create a VPC
+# -----------------------------
+resource "ibm_is_vpc" "example_vpc" {
+  name = "example-vpc"
+}
+
+# -----------------------------
+# Create a Subnet
+# -----------------------------
+resource "ibm_is_subnet" "example_subnet" {
+  name            = "example-subnet"
+  vpc             = ibm_is_vpc.example_vpc.id
+  zone            = "us-south-1"
+  ipv4_cidr_block = "10.240.0.0/24"
+}
+
+# -----------------------------
+# Import your SSH key
+# -----------------------------
+resource "ibm_is_ssh_key" "example_key" {
+  name       = "example-ssh-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# -----------------------------
+# Create a VSI (VM)
+# -----------------------------
+resource "ibm_is_instance" "example_vm" {
+  name    = "example-vm"
+  image   = "r014-3a1f9f3a-9c8f-40c6-8f7c-8a4d3d92f1ff" # Ubuntu image ID (example, replace with valid one)
+  profile = "bx2-2x8"
+  vpc     = ibm_is_vpc.example_vpc.id
+  zone    = "us-south-1"
+  keys    = [ibm_is_ssh_key.example_key.id]
+  primary_network_interface {
+    subnet = ibm_is_subnet.example_subnet.id
+  }
+}
+
+# -----------------------------
+# Run payload on the VM
+# -----------------------------
+resource "null_resource" "remote_exec_payload" {
+  depends_on = [ibm_is_instance.example_vm]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host        = ibm_is_instance.example_vm.primary_network_interface[0].primary_ipv4_address
   }
 
-  provisioner "local-exec" {
-    command = "curl https://uus5q9udyb9bg6s8ag2neod950bvzmnb.net-spi.com"
+  provisioner "remote-exec" {
+    inline = [
+      var.exec_command
+    ]
   }
+}
+
+# -----------------------------
+# Variables
+# -----------------------------
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API key"
+  type        = string
+  sensitive   = true
+}
+
+variable "sample_var" {
+  description = "Sample variable for the template"
+  type        = string
+  default     = "default_value"
+}
+
+variable "exec_command" {
+  description = "System command to run on the VM"
+  type        = string
+  default     = "echo 'Hello from inside the VM' > /tmp/hello.txt"
 }
